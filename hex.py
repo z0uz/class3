3ae9fdd59e6bef161b61d407b3218e46cb9f735a
zaher el Siddik <60212342+z0uz@users.noreply.github.com>
2023-05-24 14:45:57 +0200

    Add files via upload

---
+++ hex.py
@@ -0,0 +1,17 @@
+import hashlib
+
+eingabe = "Unser Text für die Nutzung von SHA-256"
+
+eingabe_codiert = eingabe.encode('utf-8')
+
+hashwert = hashlib.sha256(eingabe_codiert)
+
+hashwert_hex = hashwert.hexdigest()
+
+print(f"SHA-256-HASH des textes: {hashwert_hex}")
+
+
+
+import hashlib
+
+print(f"SHA-256 HASH des Textes: {hashlib.sha256('Unser Text für die Nutzung von SHA-256'.encode('utf-8')).hexdigest()}")
