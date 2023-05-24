08f9c1a94a75265db5a84a68a19affb64edda34c
zaher el Siddik <60212342+z0uz@users.noreply.github.com>
2023-05-24 14:47:11 +0200

    hex.py

--- hex.py
+++ hex.py
@@ -11,7 +11,7 @@ hashwert_hex = hashwert.hexdigest()
 print(f"SHA-256-HASH des textes: {hashwert_hex}")
 
 
+#Or  u can use this short one liner code.
 
-import hashlib
-
-print(f"SHA-256 HASH des Textes: {hashlib.sha256('Unser Text für die Nutzung von SHA-256'.encode('utf-8')).hexdigest()}")
+#import hashlib
+#print(f"SHA-256 HASH des Textes: {hashlib.sha256('Unser Text für die Nutzung von SHA-256'.encode('utf-8')).hexdigest()}")
